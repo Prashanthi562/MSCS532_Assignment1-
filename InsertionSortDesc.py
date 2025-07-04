a883ac8b6e094b3899ade554ec6dace663f60696
prashanthi562 <ppakalapati40094@ucumberlans.edu>
2025-07-04 15:24:22 -0500

    Commit 2

--- InsertionSortDesc.py
+++ InsertionSortDesc.py
@@ -1,3 +1,9 @@
+def insertion_sort_desc(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+      
 # Get input from user
 input_str = input("Enter numbers separated by spaces: ")
 arr = list(map(int, input_str.strip().split()))

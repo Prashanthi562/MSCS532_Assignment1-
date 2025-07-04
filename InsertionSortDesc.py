68963e78a3ea8d8ac73d5e0c65b9390b2550e2bc
prashanthi562 <ppakalapati40094@ucumberlans.edu>
2025-07-04 15:27:55 -0500

    Commiting entire program with tested and verifed

--- InsertionSortDesc.py
+++ InsertionSortDesc.py
@@ -1,10 +1,17 @@
+# Intialize function
 def insertion_sort_desc(arr):
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
 
+        # Move elements that are smaller than key to one position ahead
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
       
-# Get input from user
+# Get input from user n no inputs can be provided 
 input_str = input("Enter numbers separated by spaces: ")
 arr = list(map(int, input_str.strip().split()))
 
